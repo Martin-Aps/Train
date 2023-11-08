d794c37eb38e8840eff629468e0b006e24684348
Martin-Aps <149533017+Martin-Aps@users.noreply.github.com>
2023-11-08 12:56:29 +0330

    Add files via upload

---
+++ BMM.py
@@ -0,0 +1,23 @@
+number1=int(input('Enter Number1:'))
+number2=int(input('Enter Number2:'))
+if number1>number2:
+    
+    rem=number1%number2
+   
+    while True:
+        if rem !=0:
+            number2==number1
+            rem==number2
+            rem=number2%rem
+       
+        else:
+            print(number2%rem)
+            break
+   
+       
+     
+       
+
+
+
+     
